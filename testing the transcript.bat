6517f9fcd855a0d223b00414567b8612da0488ca
GODLIKE5 <fmelio1020@gmail.com>
2022-07-16 02:00:26 -0400

    Test
    Testing

--- testing the transcript.bat
+++ testing the transcript.bat
@@ -1,5 +1,5 @@
 :1
-move G:\Downloads\godlike5 - Ticket Bot\transcripts\*.* G:\godlike5.github.io\transcripts
+move G:\Downloads\godlike5 - Ticket Bot\transcripts\ G:\godlike5.github.io\transcripts
 git add .
 git commit -m "Test" -m "Testing"
 git push origin master
